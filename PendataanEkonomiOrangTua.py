66518d791335b2534555bc713221d1f279431970
Ahmad Zakaria Fathoni <60414357+azarafath@users.noreply.github.com>
2020-12-13 08:48:27 +0700

    Update PendataanEkonomiOrangTua.py

--- PendataanEkonomiOrangTua.py
+++ PendataanEkonomiOrangTua.py
@@ -1,1 +1,53 @@
+import pandas as pd
 
+# inisialisasi class
+class Orangtua():
+    
+    def __init__(self):
+        # constructor class
+        self.data =[]
+        self.jumlah = 0
+        
+    def tambah_data(self, nama, jabatan, gaji):
+        """
+        Menambah data pada list objek
+        """
+        self.data.append({'Nama':nama,
+                    'Jabatan':jabatan,
+                    'Gaji':gaji,
+                    'Kategori':''})
+        self.jumlah += 1
+        
+    def hitung_tingkat(self, pengeluaran):
+        """
+        Menghitung tingkat kesejahteraan berdasarkan
+        hasil pengurangan gaji dengan rata-rata pengeluaran/bulan
+        """
+        
+        index = self.jumlah - 1
+        
+        if (self.data[index]['Gaji'] - pengeluaran >= 1000000):
+            self.data[index]['Kategori'] = 'Sangat mampu'
+            
+        elif (self.data[index]['Gaji'] - pengeluaran >= 500000):
+            self.data[index]['Kategori'] = 'Mampu'
+            
+        elif (self.data[index]['Gaji'] - pengeluaran == 0):
+            self.data[index]['Kategori'] = 'Berkecukupan'
+            
+        else:
+            self.data[index]['Kategori'] = 'Kurang mampu'
+            
+    def tampilkan_data(self):
+        """
+        Menampilkan data dari Orangtua
+        """
+        df = pd.DataFrame(self.data)
+        
+        print("="*50)
+        print("Data Karyawan".center(50,' '))
+        print("="*50)
+        print(df)
+        print("="*50)
+        
+        print()
